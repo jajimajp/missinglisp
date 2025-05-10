5fd4320671ffa8892dfab5a16b921e8146a44972
Yajima <119467077+jajimajp@users.noreply.github.com>
2025-05-10 15:52:42 +0900

    Initialize repository

---
+++ lib/missinglisp.rb
@@ -0,0 +1,174 @@
+class Lisp
+  def initial_env
+    {
+      car: ->(*ls) { ls[0][0] },
+      cdr: ->(*ls) { ls[0].drop 1 },
+      cons: ->(e, cell, *) { [e] + cell },
+      list: ->(*ls) { ls },
+      add: ->(*ls) { ls.sum },
+      sub: ->(hd, *tl) { tl.reduce(hd) { |sum, v| sum - v } },
+      mult: ->(*ls) { ls.reduce(:*) },
+      div: ->(hd, *tl) { tl.reduce(hd) { |sum, v| sum / v } },
+
+      eq: ->(a, b, *) { a == b },
+      lt: ->(a, b, *) { a < b },
+      leq: ->(a, b, *) { a <= b },
+      gt: ->(a, b, *) { a > b },
+      geq: ->(a, b, *) { a >= b },
+
+      p: ->(*ls) { p ls },
+
+      nil: []
+    }
+  end
+
+  def eval(exp, env)
+    return exp if exp.is_a? Numeric
+    return env[exp] if exp.is_a? Symbol
+
+    # list
+    # Special forms
+    if exp[0] == :quote
+      return exp[1..]
+    elsif exp[0] == :if
+      _, test, e1, e2 = exp
+      test = self.eval(test, env)
+      return self.eval(e1, env) if test.is_a?(Array) && !test.empty?
+      return self.eval(e1, env) unless test.is_a?(Array) || !test
+
+      return self.eval(e2, env)
+    elsif exp[0] == :define
+      _, var, e = exp
+      env[var] = self.eval(e, env)
+      return []
+    elsif exp[0] == :lambda
+      _, params, e = exp
+      return ->(*args) { self.eval(e, env.merge(Hash[params.zip(args)])) }
+    end
+
+    args = exp[1..].map { |c| self.eval(c, env) }
+    env[exp.first]
+      .call(*args)
+  end
+
+  def parse(program)
+    tokens = tokenize program
+    _parse tokens
+  end
+
+  def _parse(tokens)
+    return if tokens.empty?
+
+    token = tokens.shift
+
+    case token
+    when :L
+      list = []
+
+      while tokens.first != :J
+        raise 'unexpected end of input' if tokens.empty?
+
+        list << _parse(tokens)
+      end
+      tokens.shift
+
+      list
+    when :J
+      raise 'unexpected \'J\''
+    else
+      token
+    end
+  end
+
+  def tokenize(s)
+    res = []
+    cur = ''
+    is_reading_digit = false
+    s.each_char do |c|
+      case c
+      when '_'
+        if cur != ''
+          if is_reading_digit
+            res.push cur.to_i
+          else
+            res.push cur.to_sym
+          end
+
+          cur = ''
+          is_reading_digit = false
+        end
+      when 'L'
+        if cur != ''
+          if is_reading_digit
+            res.push cur.to_i
+            cur = ''
+            is_reading_digit = false
+            res.push :L
+            next
+          end
+
+          cur += 'L' and next
+        end
+
+        res.push :L
+      when 'J'
+        if cur != ''
+          if is_reading_digit
+            res.push cur.to_i
+            cur = ''
+            is_reading_digit = false
+            res.push :J
+            next
+          end
+
+          cur += 'J' and next
+        end
+
+        res.push :J
+      when '0'..'9'
+        cur += c and next if cur != ''
+
+        is_reading_digit = true
+        cur = c
+      else
+        if is_reading_digit
+          res.push cur.to_i
+          cur = ''
+          is_reading_digit = false
+        end
+
+        cur += c
+      end
+    end
+
+    res.push cur.to_sym if cur != ''
+
+    res
+  end
+end
+
+module Kernel
+  # For irb
+  WHITE_LIST = %I[gets to_ary to_io to_str to_hash translate]
+  alias_method :base_method_missing, :method_missing
+
+  def eval_lisp(s)
+    @l ||= Lisp.new
+    @e ||= @l.initial_env
+    @l.eval(@l.parse(s), @e)
+  end
+
+  def method_missing(m, *args, &block)
+    if WHITE_LIST.include? m
+      base_method_missing(m, args, block)
+    end
+
+    eval_lisp m.to_s
+  end
+end
+
+class Object
+  def self.const_missing(id)
+    eval_lisp id.to_s
+  end
+end
---
+++ missinglisp.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name     = "missinglisp"
+  s.version  = "0.0.0"
+  s.summary  = "Lisp in Ruby variable names"
+  s.authors  = ["Yajima Soichi"]
+  s.email    = "jajima.jp@gmail.com"
+  s.files    = ["lib/missinglisp.rb"]
+  s.homepage = "https://github.com/jajimajp/missinglisp"
+  s.license  = "MIT"
+end
